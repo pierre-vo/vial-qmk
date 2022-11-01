909b307ddd837ab7b2a73cebd6b5ec4c350ec9c1
Pierre V <746601+pierre-vo@users.noreply.github.com>
2022-11-01 15:23:08 +0100

    feat(let's split eh): add vial keymap

---
+++ keyboards/maple_computing/lets_split_eh/keymaps/vial/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// place overrides here
+
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_LAYERS
+
+#define TAPPING_TERM 200
+
+#define LONGPRESS_DELAY 150
+#define LAYER_TOGGLE_DELAY 300
+#define LAYER_SKIP_DELAY 1000
+
+#define HSV_MINE_QW  10, 255, 160
+#define HSV_MINE_LW 143, 255, 180
+#define HSV_MINE_RS   0, 255, 200 //HSV_RED
+#define HSV_MINE_FN   5, 255, 200
+#define HSV_MINE_AD  80, 255, 200
+
+#define VIAL_KEYBOARD_UID {0xFA, 0x3A, 0xA0, 0x6F, 0x21, 0x69, 0x5C, 0x37}
